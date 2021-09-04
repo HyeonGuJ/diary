d4461eded86f2401f72773f8ec64e116fb176aac
hyeonguj <jhg3182@naver.com>
2021-09-05 01:24:47 +0900

    change port, welcome url

--- src/main/kotlin/com/hyeonguj/diary/sample/SampleRestController.kt
+++ src/main/kotlin/com/hyeonguj/diary/sample/SampleRestController.kt
@@ -3,11 +3,11 @@ package com.hyeonguj.diary.sample
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
-@RestController
+@RestController("/api")
 class SampleRestController {
 
-    @GetMapping
-    fun sampleController() : String {
-        return "hello";
+    @GetMapping("/welcome")
+    fun sampleController(): String {
+        return "hello! - From BackEnd";
     }
 }
